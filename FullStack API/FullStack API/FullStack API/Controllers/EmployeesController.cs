fc46a26a8bb677be60e312dd81c93ef31c954107
Esrat Jahan Mim <esratjahanmimcse67@gmail.com>
2023-04-02 22:10:12 +0600

    first commit

---
+++ FullStack API/FullStack API/FullStack API/Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using FullStack_API.Data;
+using FullStack_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack_API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+
+        public EmployeesController(FullStackDbContext fullStackDbContext)
+        {
+            _fullStackDbContext = fullStackDbContext;
+        }
+
+       
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+           var employee= await _fullStackDbContext.employees.ToListAsync();
+            return Ok(employee);
+        }
+
+        [HttpPost]  
+
+        public async Task<IActionResult> AddEmployee(Employee employee)
+        {
+            employee.Id =Guid.NewGuid();
+            await _fullStackDbContext.employees.AddAsync(employee);
+            await _fullStackDbContext.SaveChangesAsync();
+            return Ok(employee); 
+        }
+    }
+}
